3d595b196dbc588963acd79e13289c9a4db5d933
khockenj <slyster1010@gmail.com>
2018-12-02 22:41:01 -0500

    changed controller

--- app/controller.py
+++ app/controller.py
@@ -9,17 +9,15 @@ from haversine import haversine
 
 API_KEY = "515fc507675f2491dad273e15c9d742c"
 
-stop = "1 Av" #stop
-posted_ids = []
-posted_id = 0
+route = "1" #POST THE LINE HERE
+stations = []
 with open('app/data/stations.csv', newline='') as file:
 	reader = csv.reader(file, delimiter=',')
 	for line in reader:
-		if line[5] == stop:
-			posted_ids = line[7].split(" ")
+		if route in line[7].split(" "):
+			stations.append(line[2])
 
-base_id = posted_ids[0]
-api_id = str(api_information.find_id(base_id))
+api_id = str(api_information.find_id(route))
 
 base_url = 'http://datamine.mta.info/mta_esi.php?key=' + API_KEY + '&feed_id=' + api_id
 response = requests.get(base_url)
@@ -27,10 +25,9 @@ feed = gtfs_realtime_pb2.FeedMessage()
 feed.ParseFromString(response.content)
 subway_feed = protobuf_to_dict(feed)
 realtime_data = subway_feed['entity']
-station = "L11"
-dir = "N"
-station2 = "L14"
-dir2 = "N"
+NORTH = "N"
+SOUTH = "S"
+
 def getTimes(realtime_data,station,dir):
 	arrivalTimes = []
 	for choochoo in realtime_data:
@@ -40,40 +37,41 @@ def getTimes(realtime_data,station,dir):
 			#This is the format at this point, and all the information we have
 			for schedule in trips['stop_time_update']:
 				if schedule.get('stop_id') == (station+dir):
-					arrivalTimes.append(schedule['arrival']['time'])
+					if "arrival" in schedule:
+						arrivalTimes.append(schedule['arrival']['time'])
+					else:
+						arrivalTimes.append(schedule['departure']['time'])
 					#arrivalTime = time.strftime('%m/%d/%Y %I:%M%p', time.localtime(arrivalTime))
 	return arrivalTimes
+	
+def returnInfo(realtime_data,station): 
+	with open('app/data/routes/' + route + '.csv', newline='') as csvfile:
+		info = list(csv.reader(csvfile, delimiter=','))
+	base,basecoords = None,None
+	stops = []
+	longlat = []
+	for row in info:
+		if row[1] == station:
+			base = row[3]
+			baseCoords = (float(row[8]), float(row[9]))
+	for row in info:
+		if row[3] == base:
+			if dir == "S" and baseCoords[0] > float(row[8]):
+				stops.append(row[4])
+			elif dir == "N" and baseCoords[0] < float(row[8]):
+				stops.append(row[4])
 
-with open('app/data/routes/' + base_id + '.csv', newline='') as csvfile:
-	info = list(csv.reader(csvfile, delimiter=','))
-base,basecoords = None,None
-stops = []
-longlat = []
-for row in info:
-	if row[1] == station:
-		base = row[3]
-		baseCoords = (float(row[8]), float(row[9]))
-for row in info:
-	if row[3] == base:
-		if dir == "S" and baseCoords[0] > float(row[8]):
-			stops.append(row[4])
-		elif dir == "N" and baseCoords[0] < float(row[8]):
-			stops.append(row[4])
+	times1 = getTimes(realtime_data, station, NORTH)
+	times2 = getTimes(realtime_data, station, SOUTH)
+	#setTime = times1[0]
+	#d = haversine(longlat[0], longlat[1], unit="mi")
+	#coords = [longlat[0], longlat[1]]
+	
+	n = [time.strftime('%m/%d/%Y %I:%M%p', time.localtime(times)) for times in times1]
+	s = [time.strftime('%m/%d/%Y %I:%M%p', time.localtime(times)) for times in times2]
+	final = [n, s]
+	return final
 
-	if row[1] == station or row[1] == station2:
-		longlat.append((float(row[8]),float(row[9])))
-
-times1 = getTimes(realtime_data, station,dir)
-times2 = getTimes(realtime_data, station2, dir2)
-setTime = times1[0]
-d = haversine(longlat[0], longlat[1], unit="mi")
-
-for times in times2:
-	t = (times-setTime)/(60**2)
-	if setTime < times and d/t <= 30:
-		print("You will arrive from your " + time.strftime('%m/%d/%Y %I:%M%p', time.localtime(setTime)) + " train at your destination at: " + time.strftime('%m/%d/%Y %I:%M%p', time.localtime(times)))
-		break
-print(str(d) + " miles between stops")
-print("This train stops at: ")
-print(stops)
+for station in stations:
+	print(returnInfo(realtime_data,station))
 		

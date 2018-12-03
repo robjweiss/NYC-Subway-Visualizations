25a4475857cf06056d5f5666e292f4e68c5c235d
khockenj <slyster1010@gmail.com>
2018-12-02 20:30:11 -0500

    small change

--- app/__init__.py
+++ app/__init__.py
@@ -25,11 +25,31 @@ def create_app(config_name):
 				output[route].append([l for l in line[1:]])
 	for k,v in output.items():
 		k2 = re.sub(r'/', '+', k)
-		with open("app/data/" + k2 + ".csv", mode="w") as r:
+		with open("app/data/stops/" + k2 + ".csv", mode="w") as r:
 			route_writer = csv.writer(r, delimiter=',', lineterminator='\n')
 			route_writer.writerow(header)
 			for val in v:
 				route_writer.writerow(val)
+				
+	output = {}
+	header = []
+	with open('app/data/stations.csv') as stations:
+		csv_reader = csv.reader(stations, delimiter=',')
+		header = next(csv_reader, None)[1:]
+		for line in csv_reader:
+			routes = line[7].split(" ")
+			for route in routes:
+				if route not in output:
+					output[route] = [[l for l in line[1:]]]
+				else:
+					output[route].append([l for l in line[1:]])
+	for k,v in output.items():
+		with open("app/data/routes/" + k + ".csv", mode="w") as r:
+			route_writer = csv.writer(r, delimiter=',', lineterminator='\n')
+			route_writer.writerow(header)
+			for val in v:
+				route_writer.writerow(val)
+
     # Routes
 	@app.route('/')
 	def index():
--- app/controller.py
+++ app/controller.py
@@ -9,18 +9,27 @@ from haversine import haversine
 
 API_KEY = "515fc507675f2491dad273e15c9d742c"
 
-posted_id = "1" #Route
-api_id = str(api_information.find_id(posted_id))
+stop = "1 Av" #stop
+posted_ids = []
+posted_id = 0
+with open('app/data/stations.csv', newline='') as file:
+	reader = csv.reader(file, delimiter=',')
+	for line in reader:
+		if line[5] == stop:
+			posted_ids = line[7].split(" ")
+
+base_id = posted_ids[0]
+api_id = str(api_information.find_id(base_id))
 
 base_url = 'http://datamine.mta.info/mta_esi.php?key=' + API_KEY + '&feed_id=' + api_id
 response = requests.get(base_url)
 feed = gtfs_realtime_pb2.FeedMessage()
 feed.ParseFromString(response.content)
-subway_feed = protobuf_to_dict(feed) # subway_feed is a dictionary
+subway_feed = protobuf_to_dict(feed)
 realtime_data = subway_feed['entity']
-station = "111"
+station = "L11"
 dir = "N"
-station2 = "101"
+station2 = "L14"
 dir2 = "N"
 def getTimes(realtime_data,station,dir):
 	arrivalTimes = []
@@ -35,7 +44,7 @@ def getTimes(realtime_data,station,dir):
 					#arrivalTime = time.strftime('%m/%d/%Y %I:%M%p', time.localtime(arrivalTime))
 	return arrivalTimes
 
-with open('app/data/' + posted_id + '.csv', newline='') as csvfile:
+with open('app/data/routes/' + base_id + '.csv', newline='') as csvfile:
 	info = list(csv.reader(csvfile, delimiter=','))
 base,basecoords = None,None
 stops = []
